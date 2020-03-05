e1b0b4b50e295452fa125c4ad13c7fec737f961c
Paulo Sergio <paulo.zti@gmail.com>
2020-03-05 17:52:04 -0300

    Criado barra de mana e iniciado logica do dash

---
+++ Assets/Scripts/ManaBar.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+    public delegate void ManaRefilled();
+    public static event ManaRefilled OnManaFull;
+
+    public Image bar;
+    public int refillAmountPerSecond = 1;
+    public int dashCost = 100;
+    private bool refillMana = false;
+    private bool refillingMana = false;
+    private float mana;
+    void Start()
+    {
+        GameController.checkNull(bar, "Image Bar",  gameObject);
+        mana = 100;
+        CharacterMoviment.OnManaUse += UseMana;
+    }
+    private void Update()
+    {
+        bar.fillAmount = mana/100;
+        
+    }
+
+    public void UseMana()
+    {
+        mana -= dashCost;
+        refillMana = true;
+        if(!refillingMana)
+            StartCoroutine("RefillMana");
+    }
+
+    IEnumerator RefillMana()
+    {
+        while (refillMana)
+        {
+            if (mana < 100)
+            {
+                refillingMana = true;
+                mana += refillAmountPerSecond;
+                bar.fillAmount = mana/100;
+            }
+            else
+            {
+                refillMana = false;
+                refillingMana = false;
+                OnManaFull?.Invoke();
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CharacterMoviment.OnManaUse -= UseMana;
+    }
+}
