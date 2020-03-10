dcaaccad4152f85dd0151bc8bec07e56c1051664
Fabricio Kehdi Vigneron Ferreira - 519118196 <519118196@animaedu.intranet>
2020-03-10 16:09:13 -0300

    tentando criar dano, física da bola aplicada

---
+++ Assets/Damage_Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage_Player : MonoBehaviour
+{
+    public int health = 3;
+
+    public GameObject death;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(death, transform.position, Player1.tag);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Dodgeball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dodgeball : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 1;
+    public Rigidbody2D force;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        force.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+
+        Debug.Log("Batata");
+        Player2 player2 = hitInfo.GetComponent<Player2>();
+        if (player2 != null)
+        {
+            player2.TakeDamage(damage); 
+        
+        }
+        Destroy(gameObject);   
+    }
+}
+    
