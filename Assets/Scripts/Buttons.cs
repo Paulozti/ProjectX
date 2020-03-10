80dd40d8487e41d335c7058f17257d32ec48e0d8
Paulo Sergio <paulo.zti@gmail.com>
2020-03-10 19:02:19 -0300

    Criado startMenu

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
